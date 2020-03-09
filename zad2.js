4840e1c6a4d5994523051f54e57ca4585fb0c911
Madaless <bartoszmarciszewski555@gmail.com>
2020-03-09 18:00:52 +0100

    elo

--- zad2.js
+++ zad2.js
@@ -1,42 +1,43 @@
-const zad2Kodowanie = function(){
+const zad2Kodowanie = function () {
     let slowo = document.getElementById("inputZad2").value;
     let slowoWTablicy = slowo.split('');
     let Klucz = document.getElementById("numberNZad2").value;
-   
-    let dlugosc = slowo.length;
-    let KOD="";
-    let macierz= new Array(dlugoscKlucza)
-    let wartosciKlucza = Klucz.split('-') 
-    let dlugoscKlucza=wartosciKlucza.length;
 
-    for(let i=0;i<dlugoscKlucza;i++){
-        macierz[i]=[];
+    let dlugosc = slowo.length;
+    let KOD = "";
+    let wartosciKlucza = Klucz.split('-')
+    let dlugoscKlucza = wartosciKlucza.length;
+    let macierz = new Array(dlugoscKlucza);
+
+
+    for (let i = 0; i < dlugoscKlucza; i++) {
+        macierz[i] = [];
     }
 
-    let wskaznik=-1;
-    for(let j=0;j<dlugosc;j++){// tworzymy macierz 
+    let wskaznik = -1;
+    for (let j = 0; j < dlugosc; j++) {// tworzymy macierz 
         wskaznik++;
         macierz[wskaznik].push(slowoWTablicy.shift());
-        if(wskaznik==dlugoscKlucza-1)
-            wskaznik=-1;// resetowanie wskaznika
+        if (wskaznik == dlugoscKlucza - 1)
+            wskaznik = -1;// resetowanie wskaznika
     }
-    
-   
+
+
 
     let znak;
-    wskaznik=-1;// przelatujemy przez macierz i pobieramy konkretne liteki według klucza
-    for(let l=0; l<dlugosc;l++){
+    wskaznik = -1;// przelatujemy przez macierz i pobieramy konkretne liteki według klucza
+    for (let l = 0; l < dlugosc; l++) {
 
         wskaznik++;
-        znak = macierz[wartosciKlucza[wskaznik]-1].shift();
-        if(znak!=undefined)
-            KOD+=znak;
-        else{
+        znak = macierz[wartosciKlucza[wskaznik] - 1].shift();
+        if (znak != undefined)
+            KOD += znak;
+        else {
             l--;
         }
 
-        if(wskaznik==dlugoscKlucza-1)
-            wskaznik=-1;
+        if (wskaznik == dlugoscKlucza - 1)
+            wskaznik = -1;
     }
 
     // tu kod napisać
@@ -45,53 +46,52 @@ const zad2Kodowanie = function(){
 
 
 
-const zad2Odszyfrowanie = function(){
+const zad2Odszyfrowanie = function () {
     let slowo = document.getElementById("inputOdszZad2").value;
     let tablicaZnakow = slowo.split('');
     let dlugosc = slowo.length;
     let klucz = document.getElementById("numberNOdszZad2").value
-    
+
     let WYNIK = ""
-    let tablicaPomocnicza= new Array(dlugoscKlucza)
-
-
     let wartosciKlucza = klucz.split('-')
-    let dlugoscKlucza= wartosciKlucza.length;
+    let dlugoscKlucza = wartosciKlucza.length;
+    let tablicaPomocnicza = new Array(dlugoscKlucza)
+
 
     let znak;
-    let iloscWierszy = Math.ceil(dlugosc/dlugoscKlucza);
-    for(let i=0;i<iloscWierszy; i++){// zapełniamy macierz całymi wierszami
-        if(i!=iloscWierszy-1){
-            for(let j=0; j<dlugoscKlucza; j++ ){
-                tablicaPomocnicza[wartosciKlucza[j]-1] = tablicaZnakow.shift();
+    let iloscWierszy = Math.ceil(dlugosc / dlugoscKlucza);
+    for (let i = 0; i < iloscWierszy; i++) {// zapełniamy macierz całymi wierszami
+        if (i != iloscWierszy - 1) {
+            for (let j = 0; j < dlugoscKlucza; j++) {
+                tablicaPomocnicza[wartosciKlucza[j] - 1] = tablicaZnakow.shift();
             }
 
-            for(let k=0; k<dlugoscKlucza; k++){
+            for (let k = 0; k < dlugoscKlucza; k++) {
                 znak = tablicaPomocnicza[k];
-                if(znak!=undefined)
-                    WYNIK +=znak;
+                if (znak != undefined)
+                    WYNIK += znak;
             }
         }
-        else{
+        else {
             let iloscResztki = tablicaZnakow.length;// pokazuje ile literek zostalo
-            for(let j=0; j<dlugoscKlucza; j++ ){
-                if(wartosciKlucza[j]>iloscResztki){
-                    tablicaPomocnicza[wartosciKlucza[j]-1] = undefined;
+            for (let j = 0; j < dlugoscKlucza; j++) {
+                if (wartosciKlucza[j] > iloscResztki) {
+                    tablicaPomocnicza[wartosciKlucza[j] - 1] = undefined;
                     // jezeli wartosc klucza jest wieksza niz ilosc literek ktore zostały to daj undefined- bo na pewno tam zadnej literki nie bylo
-                }else{
-                    tablicaPomocnicza[wartosciKlucza[j]-1] = tablicaZnakow.shift();
+                } else {
+                    tablicaPomocnicza[wartosciKlucza[j] - 1] = tablicaZnakow.shift();
                 }
             }
 
-            for(let k=0; k<dlugoscKlucza; k++){
+            for (let k = 0; k < dlugoscKlucza; k++) {
                 znak = tablicaPomocnicza[k];
-                if(znak!=undefined)
-                    WYNIK +=znak;
+                if (znak != undefined)
+                    WYNIK += znak;
             }
         }
     }
 
-    
+
 
 
     //mtu kod napisać
--- zad4.js
+++ zad4.js
@@ -1,74 +1,94 @@
 function GCD(a, b) {
     while (b != 0) {
-            let t = b;
-            b = a % b;
-            a = t;
+        let t = b;
+        b = a % b;
+        a = t;
     }
     return a;
-}    
-    
-    // Euler’s Totient Function
-    function Phi(n) {
-        let result = {phi:1, coprimes: [1]};
-        for (let i = 2; i < n; i++) {
-                if (GCD(i, n) == 1) {
-                        result.phi += 1;
-                        result.coprimes.push(i);
-                }
+}
+
+// Euler’s Totient Function
+function Phi(n) {
+    let result = { phi: 1, coprimes: [1] };
+    for (let i = 2; i < n; i++) {
+        if (GCD(i, n) == 1) {
+            result.phi += 1;
+            result.coprimes.push(i);
         }
-        return result.length;
     }
+    return result.phi;
+}
+
+function mod(n, m) {
+    return ((n % m) + m) % m;
+}
+function modulo(divident, divisor) {
+    var partLength = 10;
+
+    while (divident.length > partLength) {
+        BigInt(part = divident.substring(0, partLength));
+        divident = (part % divisor) + divident.substring(partLength);
+    }
+
+    return BigInt(divident % divisor);
+}
+
 
 // let alphabet = "abcdefghijklmnopqrstuvwxyz";
 // let alphabetTable = alphabet.split('');
 
 
-function pozycjaLiterkiwAlfabecie(literka){
+function pozycjaLiterkiwAlfabecie(literka) {
     let pozycjaLiterki;
-    for(let i=0; i<alphabet.length;i++){
-        if(alphabetTable[i] == literka.toLowerCase()){
+    for (let i = 0; i < alphabet.length; i++) {
+        if (alphabetTable[i] == literka.toLowerCase()) {
             pozycjaLiterki = i;
         }
     }
     return pozycjaLiterki;
 }
 
-const zad4Kodowanie = function(){
+function potega(podstawa, wykladnik) {
+    let wynik = BigInt(1);
+
+    for (let i = 0; i < wykladnik; i++)
+        wynik *= podstawa;
+
+    return wynik;
+}
+
+const zad4Kodowanie = function () {
     let slowo = document.getElementById("inputZad4").value;
     let K1 = Number.parseInt(document.getElementById("numberNZad4").value);
     let K2 = Number.parseInt(document.getElementById("number2NZad4").value);
     let dlugosc = slowo.length;
-    let KOD="";
+    let KOD = "";
 
-    for(let i=0;i<dlugosc;i++){
-        let tmp = pozycjaLiterkiwAlfabecie(slowo[i])*K2+K1;
-        if(tmp<0)
-            KOD+=alphabetTable[alphabet.length-tmp];
-        else
-            KOD+=alphabetTable[tmp%alphabet.length];
+    for (let i = 0; i < dlugosc; i++) {
+        KOD += alphabetTable[mod((pozycjaLiterkiwAlfabecie(slowo[i]) * K2) + K1, alphabet.length)];
     }
 
     // tu kod napisać
     document.getElementById("zad4Code").innerHTML = KOD
 }
 
-
-
-const zad4Odszyfrowanie = function(){
+const zad4Odszyfrowanie = function () {
     let slowo = document.getElementById("inputOdszZad4").value;
     let tablicaZnakow = slowo.split('');
     let dlugosc = slowo.length;
     let K1 = Number.parseInt(document.getElementById("numberNOdszZad4").value);
     let K2 = Number.parseInt(document.getElementById("number2NOdszZad4").value);
     let WYNIK = ""
+    let a, b;
+    let k1dofi;
     //mtu kod napisać
 
-    for(let i=0;i<dlugosc;i++){
-        let tmp = (pozycjaLiterkiwAlfabecie(slowo[i])+(alphabet.length-K1))*Math.pow(K2,Phi(alphabet.length)-1);
-        if(tmp<0)
-            WYNIK+=alphabetTable[alphabet.length-tmp];
-        else
-            WYNIK+=alphabetTable[tmp%alphabet.length];
+    for (let i = 0; i < dlugosc; i++) {
+        k1dofi = BigInt(potega(BigInt(K2), BigInt(Phi(alphabet.length) - 1)));
+        //console.log(BigInt(potega(BigInt(17), BigInt(21))))
+        a = BigInt(BigInt(pozycjaLiterkiwAlfabecie(slowo[i]) + (alphabet.length - K1)) * k1dofi);
+        b = BigInt(alphabet.length);
+        WYNIK += alphabetTable[modulo(a, b)];
     }
 
     document.getElementById("zad4Result").innerHTML = WYNIK;
